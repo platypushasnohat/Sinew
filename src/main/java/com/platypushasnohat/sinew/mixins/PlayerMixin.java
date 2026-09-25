7ba398785749ad573b3bb383f25c4e1593dc623d
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2026-09-24 20:27:07 -0600

    air speed that doesnt work

--- src/main/java/com/platypushasnohat/sinew/mixins/PlayerMixin.java
+++ src/main/java/com/platypushasnohat/sinew/mixins/PlayerMixin.java
@@ -1,30 +1,40 @@
 package com.platypushasnohat.sinew.mixins;
 
 import com.platypushasnohat.sinew.registry.SinewAttributes;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Abilities;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
-public class PlayerMixin {
+public abstract class PlayerMixin extends LivingEntity {
 
-    @Inject(method = "getFlyingSpeed", at = @At(value = "RETURN"), cancellable = true)
+    @Shadow
+    @Final
+    private Abilities abilities;
+
+    protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    // todo: this doesn't work for some reason
+    @Inject(method = "getFlyingSpeed", at = @At(value = "RETURN"), cancellable = true, remap = false)
     protected void sinew$getFlyingSpeed(CallbackInfoReturnable<Float> cir) {
-        Player player = (Player) (Object) this;
-        if (!player.isPassenger() && !player.getAbilities().flying) {
+        if (!this.isPassenger() && !this.abilities.flying) {
             double airSpeed = 0.0D;
-            if (player.getAttribute(SinewAttributes.AIR_SPEED) != null) {
-                airSpeed = player.getAttributeValue(SinewAttributes.AIR_SPEED);
+            if (this.getAttribute(SinewAttributes.AIR_SPEED) != null) {
+                airSpeed = this.getAttributeValue(SinewAttributes.AIR_SPEED);
             }
             double airSpeedModifier = airSpeed / 10;
             if (airSpeed > 0.0D) {
-                if (player.isSprinting()) {
-                    cir.setReturnValue((float) Math.max(0.025999999F + airSpeedModifier, 0.005F));
-                } else {
-                    cir.setReturnValue((float) Math.max(0.02F + airSpeedModifier, 0.005F));
-                }
+                cir.setReturnValue((float) (this.isSprinting() ? 0.025999999F + airSpeedModifier : 0.02F + airSpeedModifier));
             }
         }
     }
